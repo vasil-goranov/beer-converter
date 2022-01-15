669f8136ec4d0452968e546e3de4fbb8ad28f636
Vasil <vasil.goranov@gmail.com>
2022-01-15 19:10:33 +0200

    Initial commit

---
+++ src/main/groovy/com/telerik/beerconverter/BeerConverterApplication.groovy
@@ -0,0 +1,13 @@
+package com.telerik.beerconverter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BeerConverterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BeerConverterApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/telerik/beerconverter/BeerConverterApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.telerik.beerconverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeerConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
